9436e9d550e7dc6d80a6ce881cbe532404e54eeb
rianlmci <77422550+rianlmci@users.noreply.github.com>
2022-09-02 23:35:31 -0600

    Implemented Binary Search Method and Tests

--- src/ceLinearBinary/Search.java
+++ src/ceLinearBinary/Search.java
@@ -1,5 +1,9 @@
 package ceLinearBinary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * //TODO
  */
@@ -35,6 +39,39 @@ public class Search {
      * (SCHOOL TIP: check if null is thrown w/o you coding it in first U Professors get mad)
      */
     public static int binary(String[] array, String key) {
-        return 0; //TODO
+
+        /*
+        * I knew about collections.sort and .contains
+        * I googled "search through collection java" ...
+        * I just wanted to see if there were search methods already existing.
+        * and I think I learned a little more than I should have for the point of this exercise.
+        * that's how I found out a binary search method already exists. Oops.
+        * I also learned that arrays can do a lot of methods that lists can.
+        * And figured it would be cleaner to cast the variable rather than declare a new list.
+        * I also figured it would be more efficient than using a loop.
+        * I'm sorry.
+        */
+
+        Arrays.sort(array);
+        if(Arrays.asList(array).contains(key)) {
+            return Arrays.binarySearch(array, key);
+        }
+    return -1;
+    }
+
+    //= = = TEST CLIENT = = =//
+    public static void main(String[] args){
+        String [] binaryTest = new String[] {"d","c","b","a"};
+        System.out.println("Binary test values: " + Arrays.toString(binaryTest));
+        System.out.println("Searching for key d..");
+        System.out.println("Expected: 3");
+        System.out.println("Actual: " + binary(binaryTest, "d"));
+
+        binaryTest = new String [] {"4","c", "2", "a"};
+        System.out.println("- - - -");
+        System.out.println("Binary test values: " + Arrays.toString(binaryTest));
+        System.out.println("Searching for key 2..");
+        System.out.println("Expected: 0");
+        System.out.println("Actual: " + binary(binaryTest, "2"));
     }
 }
--- tests/ceLinearBinary/SearchTest.java
+++ tests/ceLinearBinary/SearchTest.java
@@ -1,5 +1,6 @@
 package ceLinearBinary;
 
+import a01.A01Part1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,43 +22,91 @@ class SearchTest {
         assertEquals(expected, actual); //remember expected is ALWAYS first with assertEquals
     }
     @Test
-    void linear_keyIsLastElement() {
-        assertEquals(5, Search.linear(array, "j"));
-    }
+    void linear_keyIsLastElement() {assertEquals(5, Search.linear(array, "j"));}
 
     @Test
-    void linear_keyNotInArray() {
-        assertEquals(-1, Search.linear(array, "x"));
-    }
+    void linear_keyNotInArray() {assertEquals(-1, Search.linear(array, "x"));}
 
     @Test
     void linear_keyOccursMultipleTimes() {
-        String[] myArray = {"x", "o", "l", "b", "o", "m", "o", "e"};
-        assertEquals(1, Search.linear(myArray, "o"));
+        assertEquals(
+            1,
+            Search.linear(
+                new String [] {"x", "o", "l", "b", "o", "m", "o", "e"},
+                "o"
+            )
+        );
     }
 
     @Test
     void linear_emptyArray() {
-        String[] myArray = {};
-        assertEquals(-1, Search.linear(myArray, "o"));
+        assertEquals(-1, Search.linear(new String[] {}, "o"));
     }
 
     @Test
     void linear_isNull() {
-        String[] myArray = null;
-        assertThrows(NullPointerException.class, () -> {Search.linear(myArray, "1");});
+        assertThrows(NullPointerException.class, () -> {Search.linear(null, "1");});
     }
 
     @Test
     void linear_IntegerArray() {
-        Integer[] myArray = {4, 2, 8, 5 ,1};
-        assertEquals(2, Search.linear(myArray, 8));
+        assertEquals(
+            2,
+            Search.linear(
+                new Integer [] {4, 2, 8, 5 ,1},
+                8
+            )
+        );
     }
-
+    //1
     @Test
-    void testBinary() {
-        fail("Not Yet Implemented"); //TODO
+    void binary_throwsExceptionIfKeyOrArrayAreNull() {
+        assertThrows(NullPointerException.class, () ->
+            {Search.binary(null, null);}
+        );
     }
-
-
+    //2
+    @Test
+    void binary_scrambledAlphabetLowercase() {
+        assertEquals(
+            3,
+                Search.binary(
+                    new String [] {"d","c","b","a"},
+                    "d"
+                )
+        );
+    }
+    //3
+    @Test
+    void binary_scrambledNumbersAndLowercaseLetters() {
+        assertEquals(
+            0,
+            Search.binary(
+                new String [] {"4","c", "2", "a"},
+                "2"
+            )
+        );
+    }
+    //4
+    @Test
+    void binary_scrambledNumbersAndMixedCaseLetters() {
+        assertEquals(
+            2,
+            Search.binary(
+                new String [] {"4","C", "2", "a"},
+                "a"
+            )
+        );
+    }
+    //5
+    @Test
+    void binary_scrambledAlphabetLowercaseRepeats() {
+        assertEquals(
+            2,
+            Search.binary(
+                new String [] {"d","c","b","a","a","b"},
+                "b"
+            )
+        );
+    }
 }
