cba7c2b44d24ceeeb0239702df83fd51cd856255
rianlmci <77422550+rianlmci@users.noreply.github.com>
2022-10-27 01:27:33 -0600

    finished elementary sort CE

--- src/cePet/PetApp.java
+++ src/cePet/PetApp.java
@@ -25,9 +25,52 @@ public class PetApp {
         addPet(st, new Pet("Wold", 9, "dog"));
         addPet(st, new Pet("Stolch", 3, "dog"));
 
+        StdOut.print("= = = = = = = All pet names = = = = = = =\n|");
         for(Integer id : st.keys()){
-            StdOut.println(id + " "+ st.get(id));
+            StdOut.printf(" %s ( %d ) |", st.get(id).getName(), id);
         }
+
+        StdOut.println("\n\n= = = = = Testing Keys In Array = = = = =");
+        StdOut.printf("10 is a key in the symbol table: %b \n", st.contains(10));
+        StdOut.printf("17 is a key in the symbol table: %b \n", st.contains(17));
+        StdOut.printf("23 is a key in the symbol table: %b \n", st.contains(23));
+
+        StdOut.println("\n= = = = = Smallest Key In Array = = = = =");
+        StdOut.printf("The smallest key is %d!\n", st.min());
+
+        StdOut.println("\n= = = = Ceiling of 17 In Array = = = = =");
+        StdOut.printf("Smallest key greater than 17 is %d!\n", st.ceiling(17));
+
+        StdOut.println("\n= = = = Number of Pets In Array = = = = ");
+        StdOut.printf("The number of pets in the array is %d!\n", st.size());
+
+        //Add a 3-year-old dog named Waldi with pet ID 30 to the symbol table.
+        st.put(30, new Pet("Waldi", 2, "dog"));
+
+        StdOut.print("\n= = = = = Updated pet names = = = = = =\n|");
+        for(Integer id : st.keys()){
+            StdOut.printf(" %s ( %d ) |", st.get(id).getName(), id);
+        }
+
+        //"Find the number of pet IDs that are smaller than 25."
+        StdOut.println("\n\n= = = = = Rank of 25 In Array = = = = =");
+        StdOut.printf("Number of Pet-IDs that are smaller than 25: %d!\n", st.rank(25));
+
+        //Print the pet with the pet ID 30.
+        StdOut.println("\n= = = = Pet-ID 30 In The Array = = = = =");
+        StdOut.printf("Pet-ID #30 is %s!\n", st.get(30).getName());
+
+        //Find the second smallest pet ID and print the corresponding pet.
+        StdOut.println("\n= = = 2nd Smallest Pet-ID In The Array = = =");
+        StdOut.printf("The second smallest key is %d, %s!\n", st.select(1),st.get(st.select(1)).getName());
+
+        //Change the pet that is associated with the pet ID 30.
+        //It should not longer be called Waldi but Strolch instead.
+        st.put(30, new Pet("Strolch", 2, "dog"));
+
+        StdOut.println("\n= = = = New Pet-ID 30 In The Array = = = =");
+        StdOut.printf("Pet-ID #30 is %s!\n\n", st.get(30).getName());
+        StdOut.println(printPetTable());
     }
     public static void addPet(BinarySearchST st, Pet newPet){
         Integer newRandInt  = (StdRandom.uniform(10,30));
@@ -35,5 +78,17 @@ public class PetApp {
             newRandInt  = (StdRandom.uniform(10,30));
         }
         st.put(newRandInt, newPet);
+    }
+
+    public static String printPetTable(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("|======================================| \n");
+        sb.append("| Pet-ID |    Name    | Age |  Species |\n");
+        sb.append("|--------------------------------------| \n");
+        for (Integer id : st.keys()) {
+            sb.append(String.format("|   %d   | %10s |  %02d | %8s | \n",
+                    id, st.get(id).getName(), st.get(id).getAge(), st.get(id).getSpecies()));
+        }
+        return sb.toString();
     }
 }
